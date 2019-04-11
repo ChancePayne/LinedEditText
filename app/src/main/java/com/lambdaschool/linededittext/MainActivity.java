2dfc4669e5228d49993581be99b95a4e8c732be6
Chance Payne <chance@lambdaschool.com>
2019-04-11 11:14:02 -0600

    finish lined view and allow for programmatic construction

--- app/src/main/java/com/lambdaschool/linededittext/MainActivity.java
+++ app/src/main/java/com/lambdaschool/linededittext/MainActivity.java
@@ -1,7 +1,15 @@
 package com.lambdaschool.linededittext;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.lambdaschool.linededittext.views.LinedEditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // S04M04-10 construct a view and add it programmatically
+        /*LinedEditText view = new LinedEditText(this);
+        view.setPreviousColor(Color.MAGENTA);
+        view.setOffset(25);
+        view.setThickness(10);
+        view.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce malesuada elit sed dolor lobortis lobortis. Suspendisse potenti. Donec efficitur ornare leo, vitae placerat lorem convallis ac. Fusce prets tortor leo ac mauris. Pellentesque nec pharetra dolor. Nulla blandit tristique sem ultricies consequat. Quisque ut sem pellentesque, vehicula augue sed, malesuada libero.");
+
+        ((ViewGroup)findViewById(R.id.parent)).addView(view);*/
+
+        // S04M04-11A utilize your custom methods to adjust vie on the fly
+        /*LinedEditText view = findViewById(R.id.edit_view);
+        view.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus) {
+                    ((LinedEditText)v).setPreviousColor(Color.RED);
+                }
+            }
+        });*/
     }
 }
--- app/src/main/java/com/lambdaschool/linededittext/views/LinedEditText.java
+++ app/src/main/java/com/lambdaschool/linededittext/views/LinedEditText.java
@@ -2,43 +2,107 @@ package com.lambdaschool.linededittext.views;
 
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
-import android.util.Log;
+
+import com.lambdaschool.linededittext.R;
 
 // S04M04-1 Create class which extends a view (in this case, edit text)
 public class LinedEditText extends android.support.v7.widget.AppCompatEditText {
     private static final String TAG = "LinedEditText";
 
-    Rect rect;
+    Rect  rect;
     Paint paint;
+
+    int offset;
+    int previousColor;
 
     // S04M04-2 Added necessary constructors
     public LinedEditText(Context context) {
         super(context);
-        init();
+        init(null);
     }
 
     public LinedEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
+        init(attrs);
     }
 
     public LinedEditText(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+        init(attrs);
     }
 
     // S04M04-4 be sure to include an init method to initialize all necessary objects
-    private void init() {
+    private void init(AttributeSet attrs) {
         rect = new Rect();
         paint = new Paint();
         paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(5);
-        paint.setColor(Color.GREEN);
+
+//        S04M04-8 pass in attribute set and if not null, retrieve the data
+        if (attrs != null) {
+            TypedArray typedArray = getContext().obtainStyledAttributes(
+                    attrs,
+                    R.styleable.LinedEditText);
+
+            offset = typedArray.getInt(
+                    R.styleable.LinedEditText_line_offset,
+                    10);
+
+            paint.setStrokeWidth(
+                    typedArray.getFloat(
+                            R.styleable.LinedEditText_line_thickness,
+                            2));
+
+            paint.setColor(
+                    typedArray.getColor(
+                            R.styleable.LinedEditText_line_color,
+                            Color.RED));
+        } else {
+            paint.setStrokeWidth(5);
+            paint.setColor(Color.GREEN);
+            offset = 10;
+        }
+    }
+    // S04M04-11B - override other methods to provide custom behavior
+    @Override
+    protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
+        super.onFocusChanged(focused, direction, previouslyFocusedRect);
+        if(focused) {
+            previousColor = getColor();
+            setColor(Color.RED);
+        } else {
+            setColor(previousColor);
+        }
+    }
+
+    // S04M04-9 add getters and setters for any xml parameters as well as other data which should be retrievable
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public void setColor(int Color) {
+        paint.setColor(Color);
+    }
+
+    public int getColor() {
+        return paint.getColor();
+    }
+
+    public void setThickness(float thickness) {
+        paint.setStrokeWidth(thickness);
+    }
+
+    public float getThickness() {
+        return paint.getStrokeWidth();
     }
 
     // S04M04-3 override the ondraw method (this is called each time a new frame is drawn
@@ -49,10 +113,10 @@ public class LinedEditText extends android.support.v7.widget.AppCompatEditText {
 
 //        long start = System.nanoTime();
 
-        for(int i = 0; i < count; ++i) {
+        for (int i = 0; i < count; ++i) {
             int baseLine = getLineBounds(i, rect);
             // this will draw a line below each line of text
-            canvas.drawLine(rect.left, baseLine + 10, rect.right, baseLine + 10, paint);
+            canvas.drawLine(rect.left, baseLine + offset, rect.right, baseLine + offset, paint);
         }
 
 //        Log.i(TAG, "Draw Time: " + (System.nanoTime() - start));
